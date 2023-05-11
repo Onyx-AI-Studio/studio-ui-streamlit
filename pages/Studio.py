e045920919b5f91d2a016f82a92c1bd055b3bfad
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2023-05-11 19:27:20 +0530

    Build skeleton for pre-recorded audio input

--- pages/Studio.py
+++ pages/Studio.py
@@ -3,9 +3,14 @@ import json
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
 
+config = st.session_state['config']
+
+st.markdown(f"""<p style='text-align: right; color: rgb(9, 171, 59); font-family: monospace; font-weight: 600; letter-spacing: -0.005em; line-height: 1.2; margin-bottom: 0%;'>
+({config['llm_selected']})
+</p>""", unsafe_allow_html=True)
 st.title("Studio")
 
-config = st.session_state['config']
+
 # config = {
 #     "input_type": "Plain Text",
 #     "output_type": "Plain Text",
@@ -15,8 +20,6 @@ config = st.session_state['config']
 # }
 
 # st.button("Refresh")
-device = "cpu"
-
 
 @st.cache_resource
 def load_data(llm_selected):
@@ -32,7 +35,7 @@ def load_data(llm_selected):
 
 
 def get_llm_predictions(utterance: str):
-    print(f"Getting response from LLM for the input: {input_}")
+    print(f"Getting response from LLM for the input: {utterance}")
     response = call_studio_handler(utterance, config["llm_selected"])['result']
     if utterance in response:
         response = str(response).replace(utterance, "")
@@ -57,14 +60,48 @@ def call_studio_handler(utterance: str, llm_selected: str):
     return response
 
 
+# st.subheader(f"Model selected: ```{config['llm_selected']}```")
 if config['input_type'] == "Plain Text" and config['output_type'] == "Plain Text":
-    st.subheader(f"Model selected: ```{config['llm_selected']}```")
-    input_ = st.text_area(f"Text input:", height=400, label_visibility="collapsed", placeholder="Enter your input here...")
-    if input_:
+    prompt_ = st.text_area(f"Text input:", height=400, label_visibility="collapsed",
+                           placeholder="Enter your input here...")
+    if prompt_:
         st.write("")
         st.subheader("Output:")
-        decoded_output = get_llm_predictions(input_)
+        decoded_output = get_llm_predictions(prompt_)
         st.write(decoded_output)
+elif config['input_type'] == "Audio":
+    audio_file_ = st.file_uploader("Audio File Upload", type=["mp3", "wav"],
+                                   help="Upload an audio file of the allowed formats to process")
+
+    if audio_file_:
+        st.audio(audio_file_)
+        st.write("")
+        st.write("")
+
+        st.subheader("Transcript:")
+
+        # TODO: Get transcript from Middleware - Deepgram
+        transcript = "Yeah, as much as it's worth celebrating the first spacewalk with an all-female team, " \
+                     "I think many of us are looking forward to it just being normal. And I think if it signifies " \
+                     "anything, it is to honor the women who came before us who were skilled and qualified and didn't " \
+                     "get the same opportunities that we have today."
+        input_ = st.text_area("", height=300, label_visibility="collapsed", placeholder=transcript, disabled=True)
+        st.write("")
+        st.write("")
+
+        # Pass the transcript with prompt to process with LLM
+        st.subheader("Prompt:")
+        prompt_ = st.text_area(f"Text input:", height=150, label_visibility="collapsed",
+                               placeholder="Enter your prompt here...")
+        st.write("")
+
+        if prompt_:
+            st.write("")
+            st.subheader("Output:")
+            # TODO: To use get_llm_predictions()
+            llm_input = "Context: " + transcript + "\n\n" + prompt_
+            decoded_output = get_llm_predictions(llm_input)
+            st.write(decoded_output)
 else:
     st.header("Current Config:")
     st.write(config)
