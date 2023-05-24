bad298d9b9aefd60c5f092cb0db810bfaf25ca1a
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2023-05-24 23:03:42 +0530

    Add conversation id to Studio UI for better debugging

--- pages/04_Studio_UI.py
+++ pages/04_Studio_UI.py
@@ -238,6 +238,6 @@ else:
     st.header("Current Config:")
     st.write(config)
 
-st.markdown(f"""<p style='text-align: right; font-family: monospace; font-weight: 600; letter-spacing: -0.005em; line-height: 1.2; margin-top: 0%'>
-({config['llm_selected']})
+st.markdown(f"""<p style='text-align: right; font-family: monospace; font-weight: 600; letter-spacing: -0.005em; line-height: 1.2; margin-top: 5%'>
+[{config['llm_selected']} {st.session_state['conv_id']}]
 </p>""", unsafe_allow_html=True)
